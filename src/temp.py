81ea24dc4ecb62a79446d48c09f601ae57dce5e4
wkrhuang <714884865@qq.com>
2021-07-13 18:17:23 +0100

    Update temp.py

---
+++ src/temp.py
@@ -0,0 +1,15 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Training hyperparamters')
+parser.add_argument('rootDir', type=str, help='Directory that the model will be saved at')
+parser.add_argument('expName', type=str, help='The experiment name')
+parser.add_argument('--numEnc', default=6, type=int, help='The number of encoder layers')
+parser.add_argument('--numFC', default=3, type=int, help='The number of FC layers')
+parser.add_argument('--valDropout', default=0.3, type=float, help='Dropout value')
+
+args = parser.parse_args()
+print(args.rootDir)
+print(args.expName)
+print(args.numEnc)
+print(args.numFC)
+print(args.valDropout)
