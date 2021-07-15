1a57234511cc768c732fb953aa5c7b7b0b3971be
wkrhuang <714884865@qq.com>
2021-07-15 17:13:05 +0100

    Update main_train.py

--- src/main_train.py
+++ src/main_train.py
@@ -151,8 +151,9 @@ if __name__ == "__main__":
     batchSize = args.batchSize
     numWorker = args.numWorker
 
+    start_time_dset = time.time()
     train_loader, valid_loader = splitDataset(batchSize, trainValidSplit, numWorker, dataset)
-    print("Dataset initliased.")
+    print("Dataset init time: ", round(time.time() - start_time_dset, 5))
     '''
     Ntrain = round(trainValidSplit[0]*dataset.__len__())
     if Ntrain % batch_size == 1:
@@ -170,158 +171,157 @@ if __name__ == "__main__":
     valid_loader = DataLoader(dataset=valid, batch_size=batch_size, shuffle=True, num_workers=args.numWorker)
     '''
 
-    valDropoutList = []
-    num_layersList = [6]
-
     # rootDir = "./model/"
     rootDir = args.modelDir
     if not os.path.isdir(rootDir):
         os.mkdir(rootDir)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    for num_layers in num_layersList:
-        Nsample = dataset.__len__()
-        Nloc = 24
-        Ntime = 44
-        Nfreq = 512
-        Ncues = 5
-        # valDropout = 0.3
-        valDropout = args.valDropout
-        model = FC3(Nloc, Ntime, Nfreq, Ncues, num_layers, 8, device, 4, valDropout, False).to(device)
-        model.isDebug = False
-        dataset.isDebug = False
+    Nsample = dataset.__len__()
+    Nloc = 24
+    Ntime = 44
+    Nfreq = 512
+    Ncues = 5
+    # valDropout = 0.3
+    valDropout = args.valDropout
+    num_layers = args.numEnc
+    model = FC3(Nloc, Ntime, Nfreq, Ncues, num_layers, 8, device, 4, valDropout, False).to(device)
+    model.isDebug = False
+    dataset.isDebug = False
 
-        # num_epochs = 30
-        num_epochs = args.numEpoch
-        learning_rate = 1e-4
-        early_epoch = 10
-        early_epoch_count = 0
-        val_acc_optim = 0.0
+    # num_epochs = 30
+    num_epochs = args.numEpoch
+    learning_rate = 1e-4
+    early_epoch = 10
+    early_epoch_count = 0
+    val_acc_optim = 0.0
 
-        num_warmup_steps = 2
-        num_training_steps = num_epochs+1
-        warmup_proportion = float(num_warmup_steps) / float(num_training_steps)
+    num_warmup_steps = 2
+    num_training_steps = num_epochs+1
+    warmup_proportion = float(num_warmup_steps) / float(num_training_steps)
 
-        # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-        optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
-        # optimizer = AdamW(model.parameters())
+    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
+    # optimizer = AdamW(model.parameters())
 
-        # scheduler = get_linear_schedule_with_warmup(
-        #     optimizer, num_warmup_steps=num_warmup_steps, 
-        #     num_training_steps=num_training_steps
-        # )
-        scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=10, verbose=True)
-        criterion = nn.CrossEntropyLoss()
-        # criterion = nn.MSELoss()
+    # scheduler = get_linear_schedule_with_warmup(
+    #     optimizer, num_warmup_steps=num_warmup_steps, 
+    #     num_training_steps=num_training_steps
+    # )
+    scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.5, patience=10, verbose=True)
+    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.MSELoss()
 
-        print("Data volume: ", Nsample)
-        print("Nloc: ", Nloc)
-        # print("SNR: ", valSNRList)
-        print("Number of layers: ", num_layers)
-        print("Dropout: ", valDropout)
-        expName = "vol_"+str(Nsample)+"_loc_"+str(Nloc)+"_layer_"+str(num_layers)+"/"
-        checkpointPath = rootDir+expName
-        if not os.path.isdir(checkpointPath):
-            os.mkdir(checkpointPath)
+    # print("Data volume: ", Nsample)
+    # print("Nloc: ", Nloc)
+    # print("Number of layers: ", num_layers)
+    # print("Dropout: ", valDropout)
+    expName = "vol_"+str(Nsample)+"_loc_"+str(Nloc)+"_layer_"+str(num_layers)+"/"
+    checkpointPath = rootDir+expName
+    if not os.path.isdir(checkpointPath):
+        os.mkdir(checkpointPath)
 
-        for epoch in range(num_epochs):
-            print("\nEpoch %d, lr = %f" % ((epoch + 1), get_lr(optimizer)))
+    for epoch in range(num_epochs):
+        print("\nEpoch %d, lr = %f" % ((epoch + 1), get_lr(optimizer)))
+        
+        train_correct = 0.0
+        train_total = 0.0
+        train_sum_loss = 0.0
+        train_loss = 0.0
+        train_acc = 0.0
+        start_time_enum = time.time()
+        model.train()
+        for i, data in enumerate(train_loader, 0):
+            print("Pre loading time: ", round(time.time() - start_time_enum, 5))
             
-            train_correct = 0.0
-            train_total = 0.0
-            train_sum_loss = 0.0
-            train_loss = 0.0
-            train_acc = 0.0
+            start_time_load = time.time()
+            num_batches = len(train_loader)
+            inputs, labels = data
+            inputs, labels = Variable(inputs).to(device), Variable(labels).to(device)
+            # print("Input shape: ",inputs.shape)
+
+            print("Loading one batch time: ", round(time.time() - start_time_load, 5))
+            start_time_model = time.time()
+            outputs = model(inputs)
+            
+            # print("Ouput shape: ", outputs.shape)
+            # print("Label shape: ", labels.shape)
+            loss = criterion(outputs, labels) # .unsqueeze_(1)
+            optimizer.zero_grad()
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+            optimizer.step()
+            train_sum_loss += loss.item()
+
+            print("Model prediction time: ", round(time.time()-start_time_model, 5))
+
+            _, predicted = torch.max(outputs.data, 1)
+            train_total += labels.size(0)
+            train_correct += predicted.eq(labels.data).sum().item()
             start_time_enum = time.time()
-            model.train()
-            for i, data in enumerate(train_loader, 0):
-                print("Pre loading: ", round(time.time()-start_time_enum, 5))
-                
-                start_time_load = time.time()
-                num_batches = len(train_loader)
+        train_loss = train_sum_loss / (i+1)
+        train_acc = round(100.0 * train_correct / train_total, 2)
+        print('Training Loss: %.04f | Training Acc: %.4f%% '
+            % (train_loss, train_acc))
+        
+        val_correct = 0.0
+        val_total = 0.0
+        val_sum_loss = 0.0
+        val_loss = 0.0
+        val_acc = 0.0
+
+        # validation phase
+        model.eval()
+        with torch.no_grad():
+            for i, data in enumerate(valid_loader, 0):
                 inputs, labels = data
                 inputs, labels = Variable(inputs).to(device), Variable(labels).to(device)
-                # print("Input shape: ",inputs.shape)
-
-                print("Loading one batch: ", round(time.time() - start_time_load, 5))
+                
                 outputs = model(inputs)
-
-                # print("Ouput shape: ", outputs.shape)
-                # print("Label shape: ", labels.shape)
-                loss = criterion(outputs, labels) # .unsqueeze_(1)
-                optimizer.zero_grad()
-                loss.backward()
-                # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
-                optimizer.step()
-                train_sum_loss += loss.item()
+                val_loss = criterion(outputs, labels) # .unsqueeze_(1)
+                val_sum_loss += val_loss.item()
 
                 _, predicted = torch.max(outputs.data, 1)
-                train_total += labels.size(0)
-                train_correct += predicted.eq(labels.data).sum().item()
-                start_time_enum = time.time()
-            train_loss = train_sum_loss / (i+1)
-            train_acc = round(100.0 * train_correct / train_total, 2)
-            print('Training Loss: %.04f | Training Acc: %.4f%% '
-                % (train_loss, train_acc))
-            
-            val_correct = 0.0
-            val_total = 0.0
-            val_sum_loss = 0.0
-            val_loss = 0.0
-            val_acc = 0.0
+                val_total += labels.size(0)
+                val_correct += predicted.eq(labels.data).sum().item()
+            val_loss = val_sum_loss / (i+1)
+            val_acc = round(100.0 * val_correct / val_total, 2)
+            scheduler.step(val_loss)
 
-            # validation phase
-            model.eval()
-            with torch.no_grad():
-                for i, data in enumerate(valid_loader, 0):
-                    inputs, labels = data
-                    inputs, labels = Variable(inputs).to(device), Variable(labels).to(device)
-                    
-                    outputs = model(inputs)
-                    val_loss = criterion(outputs, labels) # .unsqueeze_(1)
-                    val_sum_loss += val_loss.item()
+        print('Val_Loss: %.04f | Val_Acc: %.4f%% '
+            % (val_loss, val_acc))
+        
+        checkpoint_curve = {
+            "epoch": epoch+1,
+            "train_loss": train_loss,
+            "train_acc": train_acc,
+            "valid_loss": val_loss,
+            "valid_acc": val_acc
+        }
+        
+        torch.save(
+            checkpoint_curve,
+            checkpointPath + "curve_epoch_"+str(epoch+1)+".pth.tar"
+        )
 
-                    _, predicted = torch.max(outputs.data, 1)
-                    val_total += labels.size(0)
-                    val_correct += predicted.eq(labels.data).sum().item()
-                val_loss = val_sum_loss / (i+1)
-                val_acc = round(100.0 * val_correct / val_total, 2)
-                scheduler.step(val_loss)
+        
 
-            print('Val_Loss: %.04f | Val_Acc: %.4f%% '
-                % (val_loss, val_acc))
-            
-            checkpoint_curve = {
+        # early stopping
+        if (val_acc <= val_acc_optim):
+            early_epoch_count += 1
+        else:
+            val_acc_optim = val_acc
+            early_epoch_count = 0
+
+            checkpoint_param = {
                 "epoch": epoch+1,
-                "train_loss": train_loss,
-                "train_acc": train_acc,
-                "valid_loss": val_loss,
-                "valid_acc": val_acc
+                "state_dict": model.state_dict(),
+                "optimizer": optimizer.state_dict()
             }
-            
+
             torch.save(
-                checkpoint_curve,
-                checkpointPath + "curve_epoch_"+str(epoch+1)+".pth.tar"
+                checkpoint_param,
+                checkpointPath + "param.pth.tar"
             )
-
-            
-
-            # early stopping
-            if (val_acc <= val_acc_optim):
-                early_epoch_count += 1
-            else:
-                val_acc_optim = val_acc
-                early_epoch_count = 0
-
-                checkpoint_param = {
-                    "epoch": epoch+1,
-                    "state_dict": model.state_dict(),
-                    "optimizer": optimizer.state_dict()
-                }
-
-                torch.save(
-                    checkpoint_param,
-                    checkpointPath + "param.pth.tar"
-                )
-            if (early_epoch_count >= early_epoch):
-                break
+        if (early_epoch_count >= early_epoch):
+            break
