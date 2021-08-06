b293c431a0ff554cd89ca44ed668765f853b0c08
wkrhuang <714884865@qq.com>
2021-08-06 10:45:18 +0100

    Create fast_vis_csv.py

---
+++ src/fast_vis_csv.py
@@ -0,0 +1,15 @@
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Fast visualise csv files')
+    parser.add_argument('csvPath', type=str, help='Path to the csv file')
+
+    args = parser.parse_args()
+    print("Path to the csv file: ", args.csvPath)
+
+    if args.csvPath[-4:] != ".csv":
+        args.csvPath += "/.csv"
+        
+    csvF = pd.read_csv(args.csvPath, header=None)
+    print(csvF)
