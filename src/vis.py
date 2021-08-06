7e7d535d3a71fb387d0e84225c5be395e8287c1f
wkrhuang <714884865@qq.com>
2021-08-06 10:58:07 +0100

    Update vis.py

--- src/vis.py
+++ src/vis.py
@@ -5,28 +5,10 @@ from glob import glob
 import os
 import torch
 import torch.nn as nn
+from scipy.fft import fft, ifft
 
-# visualise the spectrogram
-def showSpectrogram(Zxx, fs, figTitle, isLog=True):
-    fig, ax = plt.subplots()
-    print("Spectrogram shape: ", Zxx.shape)
-
-    if isLog:
-        img = librosa.display.specshow(librosa.amplitude_to_db(np.abs(Zxx),ref=np.max),
-                                    sr=fs, hop_length=512, fmax=fs/2,
-                                    y_axis='linear', x_axis='time', ax=ax)
-    else:
-        img = librosa.display.specshow(np.abs(Zxx),
-                                    sr=fs, hop_length=512, fmax=fs/2,
-                                    y_axis='linear', x_axis='time', ax=ax)
-    ax.set_title(figTitle)
-    if isLog:
-        fig.colorbar(img, ax=ax, format="%+2.0f dB")
-    else:
-        fig.colorbar(img, ax=ax, format="%+2.0f")
-    # fig.set_figheight(5)
-    # fig.set_figwidth(5)
-    plt.show()
+from utils import *
+from load_data import *
 
 def plotHistory(checkptPath):
     checkptPath = glob(os.path.join(checkptPath, "curve*"))
@@ -62,9 +44,6 @@ def plotHistory(checkptPath):
     plt.grid()
     plt.show()
 
-
-# [TODO]
-# need a method to visualise the cues
 
 if __name__ == "__main__":
     pass
