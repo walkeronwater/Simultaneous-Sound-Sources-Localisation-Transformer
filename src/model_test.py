6cf3504a9a0159c91b90ca1795a19805b085c752
wkrhuang <714884865@qq.com>
2021-08-29 15:46:38 +0800

    Update model_test.py

--- src/model_test.py
+++ src/model_test.py
@@ -290,6 +290,6 @@ if __name__ == "__main__":
                         
         loss_all_subject.append(loss_allSNR/loss_allSNR_count)
         loss_all_subject_x.append(subject_idx)
-    
-    print(loss_all_subject)
-    print(loss_all_subject_x)
+        
+        print(loss_all_subject)
+        print(loss_all_subject_x)
